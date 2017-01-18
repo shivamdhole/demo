f72136cb29146aef95a0a86d7347c85d67a14e75
author <author@example.com>
2017-01-18 12:22:03 +1100

    editing the file

--- samplemodel.py
+++ samplemodel.py
@@ -1,18 +1,1 @@
-import pandas.io.data as web
-import numpy as np
-import datetime
-
-start = datetime.datetime(2016,1,1)
-end = datetime.date.today()
-
-apple = web.DataReader("AAPL","yahoo",start,end)
-type(apple)
-print(apple.head())
-f = open('AirPassengers.csv','r')
-i = 0
-for line in f:
-	print(line)
-	i += 1
-	if i > 5:
-		break
-print('File read complete')
+#This is extra line as a second commit to this file
